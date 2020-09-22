29a0a61b8bc9a5f1d2cf4aec55c077421168bfa3
Kathan Patel <patelkathan22@gmail.com>
2020-09-22 10:01:09 +0530

    Added User-Agent
    Added User-Agent Had Some Issue With some response body.

--- gxss.go
+++ gxss.go
@@ -105,6 +105,8 @@ func testxss(p string, v bool) {
 				return
 			}
 
+			req.Header.Add("User-Agent", "User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36")
+			
 			resp, err := httpClient.Do(req)
 			if err != nil {
 				return
